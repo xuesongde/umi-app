2c25efcd608ff26780d82420a4976a30c672586d
Cowherd <185217985@qq.com>
2022-07-14 16:18:59 +0800

    Update debounce.js

--- src/utils/debounce.js
+++ src/utils/debounce.js
@@ -7,5 +7,65 @@ const debounce = (fn, wait) => {
     }, wait);
   };
 };
-
+const tree = {
+    val: 1,
+    left: {
+        val: 2,
+        left: {
+            val: 4,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 5,
+            left: {
+                val: 8,
+                left: null,
+                right: null,
+            },
+            right: {
+                val: 9,
+                left: null,
+                right: null,
+            },
+        },
+    },
+    right: {
+        val: 3,
+        left: {
+            val: 6,
+            left: null,
+            right: null,
+        },
+        right: {
+            val: 7,
+            left: null,
+            right: null,
+        },
+    },
+};
+// traverse tree by recursion
+const getThroughTree=(tree)=>{
+    const res = [];
+    const preOrder=(root)=>{
+        if(!root) return;
+        res.push(root.val);// root
+        preOrder(root.left);// left
+        preOrder(root.right);// right
+    }
+    preOrder(tree);
+    return res;
+}
+// traverse tree by loop
+const loopTravelTree=(tree)=>{
+    const res = [];
+    const stack = [tree];
+    while(stack.length){
+        const treeNode = stack.pop();
+        res.push(treeNode.val);
+        treeNode.left && stack.push(treeNode.left);
+        treeNode.right && stack.push(treeNode.right);
+    }
+    return res;
+}
 export default debounce;
